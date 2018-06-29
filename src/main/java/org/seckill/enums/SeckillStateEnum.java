36f572fcc938781e8505c290a09106c4ef9121af
pydxmg <1196923457@qq.com>
2018-06-29 16:42:17 +0800

    去掉set方法
    去掉set方法

--- src/main/java/org/seckill/enums/SeckillStateEnum.java
+++ src/main/java/org/seckill/enums/SeckillStateEnum.java
@@ -1,28 +1,22 @@
 package org.seckill.enums;
 /**
- * 使用枚举表示常量数据
+ * 浣跨敤鏋氫妇琛ㄧず甯搁噺鏁版嵁
  * @author mjf
  *
  */
 public enum SeckillStateEnum {
-	SUCCESS(1,"秒杀成功"),
-	END(0,"秒杀结束"),
-	REPEAT_KILL(-1,"重复秒杀"),
-	INNER_ERROR(-2,"系统异常"),
-	DATA_REWRITE(-3,"数据篡改");
+	SUCCESS(1,"绉掓潃鎴愬姛"),
+	END(0,"绉掓潃缁撴潫"),
+	REPEAT_KILL(-1,"閲嶅绉掓潃"),
+	INNER_ERROR(-2,"绯荤粺寮傚父"),
+	DATA_REWRITE(-3,"鏁版嵁绡℃敼");
 	private int state;
 	private String stateInfo;
 	public int getState() {
 		return state;
 	}
-	public void setState(int state) {
-		this.state = state;
-	}
 	public String getStateInfo() {
 		return stateInfo;
-	}
-	public void setStateInfo(String stateInfo) {
-		this.stateInfo = stateInfo;
 	}
 	private SeckillStateEnum(int state, String stateInfo) {
 		this.state = state;
